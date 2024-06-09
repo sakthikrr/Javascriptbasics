b6b1f98c813c1733aac40b84f8548d7320dc1b90
Sakthi <sakthivel.kss7@gmail.com>
2024-06-09 15:00:04 +0530

    variables

--- default.js
+++ default.js
@@ -1,3 +1,20 @@
 // Created on iPad.
 
 console.log("Hello, World!")
+let num = 1;
+let str ="hai";
+let un = undefined
+let nu = null
+console.log(num)
+console.log(un)
+console.log(nu)
+//if else
+if(num === 1){
+    console.log("one");
+}
+if(num === "1"){
+    console.log("one num")
+}else{
+    console.log("not one")
+}
+
