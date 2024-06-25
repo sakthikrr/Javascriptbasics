953553cb573584029efe4d521ddea6a6da672621
Sakthi <sakthivel.kss7@gmail.com>
2024-06-25 11:14:09 +0530

    arrays javascript

---
+++ javascriptarray.js
@@ -0,0 +1,8 @@
+nam_arr = ["test1","test2","test3"];
+console.log(nam_arr);
+console.log(nam_arr[1]);
+nam_arr.push("test5");
+console.log(nam_arr);
+//Remove array element 
+nam_arr.splice(2,1);
+console.log(nam_arr)
