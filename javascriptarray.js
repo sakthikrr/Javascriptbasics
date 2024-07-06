0808b52bcef72fe30aedf8472a1d83467003be48
Sakthi <sakthivel.kss7@gmail.com>
2024-07-06 13:53:57 +0530

    array map

--- javascriptarray.js
+++ javascriptarray.js
@@ -6,3 +6,17 @@ console.log(nam_arr);
 //Remove array element 
 nam_arr.splice(2,1);
 console.log(nam_arr)
+//Array Map
+const user =[
+    {"id":1,"name":"sakthi","status":true},
+    {"id":2,"name":"vel","status":false},
+    {"id":3,"name":"thanges","status":true}
+]
+const usernames = user.map(userdata=>{
+    return userdata.name;
+})
+console.log("array map");
+console.log(usernames);
+//Array filter
+const activeusers = user.filter(userdata=>userdata.status )
+console.log(activeusers);
