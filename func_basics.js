711de3fa544d5555986971d4dd4f95d70266c43e
sakthikrr <56345260+sakthikrr@users.noreply.github.com>
2024-12-14 19:41:26 +0530

    functions basics

--- func_basics.js
+++ func_basics.js
@@ -5,3 +5,20 @@ function basfunc(s){
 }
 
 basfunc("function after call");
+
+//Single line arrow function
+const multiply = (a, b) => a * b;
+console.log(multiply(2, 3)); // 6
+
+//Multi line arrow function
+const calculate = (a, b) => {
+    const sum = a + b;
+    const product = a * b;
+    return { sum, product };
+  };
+  console.log(calculate(2, 3)); 
+
+//Arrow Functions in Arrays
+const numbers = [1, 2, 3];
+const squares = numbers.map(num => num * num);
+console.log(squares); // [1, 4, 9]
