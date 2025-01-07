497b44f293fc6c11a0ab6e467306fba185da7e95
Sakthivel <sakthivel.kss7@gmail.com>
2025-01-07 10:19:42 +0530

    object desctruct

--- destruct_basics.js
+++ destruct_basics.js
@@ -19,3 +19,45 @@ const arr2 = [1,2,3,["a","b"]]
 const [a,b,, [c,d]] = arr2
 console.log("Inner array desctructure=",a,b,c,d);
 
+// Destructurin Objects
+console.log("====================")
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+const restaurant_data = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  // ES6 enhanced object literals
+  openingHours : {
+    mon: {
+      open: 12,
+      close: 22,
+    },
+    tue: {
+      open: 11,
+      close: 23,
+    },
+   wed: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  }
+
+};
+
+const { name:restaurant_name,openingHours} = restaurant_data;
+console.log("Object Desctructuring with name = ",restaurant_name);
+console.log("Object Desctructuring with openingHours = ",openingHours);
+
+let s = 7 
+let w = 10
+const obj = {s:27,w:30};
+console.log(obj);
+
+ ({s,w} = obj)
+console.log(s,w);
+
+const {openingHours:{mon:{open:op,close:cl}}} = restaurant_data;
+console.log("Monday opening =",op,"close=",cl);
